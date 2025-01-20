8086f0e0c16dabb45acabd632a117563e8aa6787
ArpKevin <arpaskevin11@gmail.com>
2025-01-20 09:38:53 +0100

    first commit

---
+++ upload.php
@@ -0,0 +1,44 @@
+    <?php
+
+    include('dbconnection.php');
+    if (isset($_POST['submit'])){
+        $file_name = $_FILES['image']['name'];
+        $temp_name = $_FILES['image']['tmp_name'];
+
+        $path = 'images/'.$file_name;
+
+        $query = mysqli_query($con,"insert into images (file) values ('$file_name')");
+
+        if (move_uploaded_file($temp_name, $path)){
+            echo '<h2>File uploaded successfully</h2>';
+        }
+        else{
+            echo '<h2>Error with upload</h2>';
+        }
+    }
+
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post" enctype="multipart/form-data" action="">
+        <input type="file" name="image" id="">
+        <br>
+        <button type="submit" name="submit">Submit</button>
+        </form>
+        <div>
+            <?php
+                $res = mysqli_query($con, "select * from images");
+                while ($row = mysqli_fetch_assoc($res)) :
+            ?>
+            <img src="images/<?= $row['file'] ?>" alt="">
+            <?php endwhile ?>
+        </div>
+    </body>
+    </html>
